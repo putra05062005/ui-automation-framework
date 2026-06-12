1aaf4ba0b1517ff226bc4d811759f0499e6ca5c8
Pahala <rftkohin@gmail.com>
2026-06-12 17:13:06 +0700

    fix driver setup inside class for CI

--- app/src/test/java/pages/LoginPage.java
+++ app/src/test/java/pages/LoginPage.java
@@ -1,9 +1,12 @@
 package pages;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class LoginPage {
@@ -16,8 +19,17 @@ public class LoginPage {
     By loginBtn = By.cssSelector("button[type='submit']");
     By successMsg = By.id("flash");
 
-    public LoginPage(WebDriver driver) {
-        this.driver = driver;
+    public LoginPage() {
+
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        this.driver = new ChromeDriver(options);
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
@@ -41,5 +53,9 @@ public class LoginPage {
         return wait.until(
             ExpectedConditions.visibilityOfElementLocated(successMsg)
         ).getText();
+    }
+
+    public void close() {
+        driver.quit();
     }
 }
